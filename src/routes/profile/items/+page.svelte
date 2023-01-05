9ad76f214c06cd52ae1656f313571e4ab64ba779
Jay Joo <jj534@cornell.edu>
2023-01-05 11:00:00 +0900

    improve thumbnail ui

--- src/routes/profile/items/+page.svelte
+++ src/routes/profile/items/+page.svelte
@@ -25,18 +25,18 @@
 				<a href={`/item/${item?.id}`}>
 					<div class="flex space-x-5 rounded p-2">
 						<img
-							class="h-[100px] rounded-xl object-cover"
+							class="h-[120px] w-[150px] flex-shrink-0 rounded-xl object-cover"
 							src={pb.getFileUrl(item, item?.photos[0], { thumb: '300x0' })}
 							alt=""
 							width="150"
-							height="100"
+							height="120"
 						/>
-						<div class="flex h-[100px] w-full flex-col justify-between">
+						<div class="flex h-[120px] w-full flex-col justify-between">
 							<div>
-								<p class="text-xl font-semibold">{item?.name}</p>
+								<p class="text-xl ">{item?.name}</p>
 								<p class="text-lg font-medium">{item?.expand?.user?.name}</p>
 							</div>
-							<p class="text-xl font-medium">${item?.price}</p>
+							<p class="text-xl font-bold">${item?.price}</p>
 						</div>
 					</div>
 				</a>
